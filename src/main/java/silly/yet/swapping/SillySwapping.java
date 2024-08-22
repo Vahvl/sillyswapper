8dea3678482bd0e04f4bedcba06235c0710d5c5a
Vahvl <74665243+Vahvl@users.noreply.github.com>
2024-08-22 13:06:58 +0300

    chatroom ion think its bannable anymore lets fucking goooo

--- src/main/java/silly/yet/swapping/SillySwapping.java
+++ src/main/java/silly/yet/swapping/SillySwapping.java
@@ -4,7 +4,6 @@ import net.minecraft.client.Minecraft;
 import net.minecraft.client.entity.EntityPlayerSP;
 import net.minecraft.entity.player.EntityPlayer;
 import net.minecraft.item.ItemStack;
-import net.minecraft.network.play.client.C09PacketHeldItemChange;
 import net.minecraftforge.fml.common.eventhandler.EventPriority;
 import net.minecraftforge.fml.common.eventhandler.SubscribeEvent;
 import net.minecraftforge.fml.common.gameevent.TickEvent;
@@ -12,12 +11,11 @@ import silly.yet.swapping.config.SillyConfig;
 
 public class SillySwapping {
 
-    //private static final long MIN_DELAY_MS = 10;  // Minimum delay in milliseconds
-    //private static final long MAX_DELAY_MS = 30;  // Maximum delay in milliseconds
     private int originalSlot = -1; // Store the original slot
     private int targetSlot = -1; // Store the slot with Etherwarp Conduit
-    private boolean wasSneaking = false;
+    private boolean keyHeld = false;
     private boolean active = false;
+
 
     @SubscribeEvent(priority = EventPriority.NORMAL, receiveCanceled = true)
     public void onTick(TickEvent.PlayerTickEvent event) {
@@ -28,37 +26,38 @@ public class SillySwapping {
         if (!player.worldObj.isRemote) return;
 
         boolean keyHeld = SillyConfig.sillyBind.isActive();
-        if (keyHeld == wasSneaking) return;
-        wasSneaking = keyHeld;
+        if (keyHeld == this.keyHeld) return;
+        this.keyHeld = keyHeld;
 
         int currentSlot = player.inventory.currentItem;
         if (keyHeld) {
-            if (SillyConfig.AOTVMode && heldItem != null && heldItem.getDisplayName().contains("Aspect Of The Void")) {
+            if (SillyConfig.AOTVMode && heldItem != null && heldItem.getDisplayName().contains("Aspect of the Void")) {
                 if (Minecraft.getMinecraft().currentScreen != null) return;
                 targetSlot = findHotbarSlotWithItem(player, "Etherwarp Conduit");
                 if (targetSlot == -1) return;
                 originalSlot = currentSlot;
                 player.inventory.currentItem = targetSlot;
                 active = true;
-                player.sendQueue.addToSendQueue(new C09PacketHeldItemChange(targetSlot));
-            } else if (!SillyConfig.AOTVMode){
+                Minecraft.getMinecraft().thePlayer.inventory.currentItem = targetSlot;
+            } else if (!SillyConfig.AOTVMode) {
                 if (Minecraft.getMinecraft().currentScreen != null) return;
                 targetSlot = findHotbarSlotWithItem(player, "Etherwarp Conduit");
                 if (targetSlot == -1) return;
                 originalSlot = currentSlot;
                 player.inventory.currentItem = targetSlot;
                 active = true;
-                player.sendQueue.addToSendQueue(new C09PacketHeldItemChange(targetSlot));
+                Minecraft.getMinecraft().thePlayer.inventory.currentItem = targetSlot;
             }
         } else {
             boolean toggle = active;
             active = false;
-            if (toggle && heldItem.getDisplayName().contains("Etherwarp Conduit")) {
+            if (heldItem != null && toggle && heldItem.getDisplayName().contains("Etherwarp Conduit")) {
                 player.inventory.currentItem = originalSlot;
-                player.sendQueue.addToSendQueue(new C09PacketHeldItemChange(originalSlot));
+                Minecraft.getMinecraft().thePlayer.inventory.currentItem = originalSlot;
             }
         }
     }
+
     private int findHotbarSlotWithItem(EntityPlayer player, String itemName) {
         for (int i = 0; i < 9; i++) {
             ItemStack stack = player.inventory.getStackInSlot(i);
@@ -67,9 +66,5 @@ public class SillySwapping {
             }
         }
         return -1;
-    }
-
-    private long getRandomDelay(long min, long max) {
-        return min + (long) (Math.random() * (max - min));
     }
 }
